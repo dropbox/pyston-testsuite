c2be219f4ec8a12c93704ed94a053a659032854c
Rudi Chen <rudichen@gmail.com>
2015-08-07 10:28:38 -0700

    Extent NumPy test to run some code.

--- integration/numpy_test.py
+++ integration/numpy_test.py
@@ -53,7 +53,57 @@ print "\n>>>"
 print ">>> Setting up NumPy..."
 print ">>>"
 subprocess.check_call([PYTHON_EXE, "setup.py", "build"], cwd=NUMPY_DIR)
+
+print "\n>>>"
+print ">>> Installing NumPy..."
+print ">>>"
 subprocess.check_call([PYTHON_EXE, "setup.py", "install"], cwd=NUMPY_DIR)
+
+# From Wikipedia
+script = """
+import numpy as np
+from numpy.linalg import solve, inv
+a = np.array([[1, 2, 3], [3, 4, 6.7], [5, 9.0, 5]])
+b = np.array([3, 2, 1])
+a.transpose()
+inv(a)
+solve(a, b)
+print a, b
+"""
+
+# http://wiki.scipy.org/Tentative_NumPy_Tutorial/Mandelbrot_Set_Example
+mandelbrot = """
+from numpy import *
+
+def mandelbrot( h,w, maxit=20 ):
+        '''Returns an image of the Mandelbrot fractal of size (h,w).
+        '''
+        y,x = ogrid[ -1.4:1.4:h*1j, -2:0.8:w*1j ]
+        c = x+y*1j
+        z = c
+        divtime = maxit + zeros(z.shape, dtype=int)
+
+        for i in xrange(maxit):
+                z  = z**2 + c
+                diverge = z*conj(z) > 2**2
+                div_now = diverge & (divtime==maxit)
+                divtime[div_now] = i
+                z[diverge] = 2
+
+        return divtime
+
+mandelbrot(complex(400),complex(400))
+"""
+
+print "\n>>>"
+print ">>> Running NumPy linear algebra test..."
+print ">>>"
+subprocess.check_call([PYTHON_EXE, "-c", script], cwd=CYTHON_DIR)
+
+print "\n>>>"
+print ">>> Running NumPy mandelbrot test..."
+print ">>>"
+subprocess.check_call([PYTHON_EXE, "-c", mandelbrot], cwd=CYTHON_DIR)
 
 print
 print "PASSED"
